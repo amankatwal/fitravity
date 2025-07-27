bd89f23224dc8392a9e00f296a609a0478c00149
Amandeep Singh <katwal004@Gmail.com>
2025-07-27 23:33:10 +0530

    added trainer assignment using round robin logic with trainer tracker.

--- server/components/query.js
+++ server/components/query.js
@@ -2,12 +2,21 @@
 // USERS QUERY
 export const getUsers = "SELECT * FROM users"
 export const userCheck = "SELECT * FROM users WHERE email = $1"
+
 export const postUser = "INSERT INTO users (email, name, password) VALUES ($1, $2, $3)"
 export const loginUser = "SELECT (email, password)"
 export const plans = "SELECT * FROM plans ORDER BY price ASC "
+export const trainerAssign = "UPDATE users SET trainer_id =$1 where email = $2";
 // TRAINERS QUERY
 
 export const postTrainer= "INSERT INTO trainer (name, email,phone_number, password ) values($1, $2, $3, $4)";
 export const TrainerCheck = "SELECT * FROM trainer WHERE EMAIL =$1";
 export const postPlan = "UPDATE users SET plan = $1 WHERE email = $2"
+export const promotionCheck = "SELECT * FROM trainer where promotion = $1"
+export const trainerTotal = "SELECT * FROM trainer ORDER BY id ASC"
 
+//trianer Tracker
+
+export const trainerTracker = "SELECT last_assigned_trainer_id FROM trainer_tracker WHERE id=1";
+export const updateTracker = "UPDATE trainer_tracker SET last_assigned_trainer_id = $1 WHERE id =1";
+const test = "UPDATE users SET plan = null";
--- server/components/userController.js
+++ server/components/userController.js
@@ -1,9 +1,9 @@
 import pool from "./db.js";
-import { postPlan } from "./query.js";
+import { postPlan, promotionCheck, trainerAssign, trainerTotal, trainerTracker, updateTracker, userCheck } from "./query.js";
 
 
 
-export const purchasePlan = async(req, res) =>{
+export const purchasePlan = async(req, res, next) =>{
     try {
          const user_email = req.user.email;
     const {plan} = req.body;
@@ -13,6 +13,35 @@ export const purchasePlan = async(req, res) =>{
     }
     const updatePlan = await pool.query(postPlan, [plan, user_email]);
     res.status(200).send("plan Purchased");
+     const email = req.user.email;
+    const {promotion} = req.body;
+    const userRes = await pool.query(userCheck, [email]);
+       
+
+    if(userRes.rows[0].trainer_id) {
+        if (userRes.rows.length > 0 && userRes.rows[0].trainer_id){
+         userRes.status.send("trainer Already assigned");
+    }}
+   let trainer_id;
+   if (promotion){
+    const result = await pool.query(promotionCheck, [promotion]);
+    if (result.rows.length > 0){
+      trainer_id = result.rows[0].id;
+    }
+   }
+   if(!trainer_id){
+    const lastTrainer = await pool.query(trainerTracker);
+    let lastTrainerID = lastTrainer.rows[0].last_assigned_trainer_id;
+    const totalTrainer = await pool.query(trainerTotal);
+    const total = totalTrainer.rows.length;
+
+    let newTrainer = (lastTrainerID +1) % total;
+     trainer_id = totalTrainer.rows[newTrainer].id;
+    await pool.query(updateTracker, [newTrainer]);
+
+   }
+
+   await pool.query(trainerAssign, [trainer_id, email]);
     } catch (err) {
         console.error(err);
     }
